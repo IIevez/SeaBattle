44f11f5ba971cadb58df21841f7d1f12261a8b8b
Алексей <136973838+IIevez@users.noreply.github.com>
2025-12-04 21:49:51 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <vector>//Мне понравилось, как был реализован вектор в коде нейронки
+using namespace std;
+//структура классов карты
+struct TheMap{
+    class CreateTheMap{
+        public:
+        private:
+    };
+    //идентификация кораблей на карте
+    class ship_identification{
+        public:
+        private:
+    };
+    //
+    //идентификация выстрела на карте
+    class shoot_identification{
+        public:
+        private:
+    };
+};
+//
+int main(){
+    cout<<"PORNO";
+    return 0;
+
+}
